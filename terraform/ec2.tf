3949c14a5483fead1fbaa8c77edfc294d676f2c0
mafunzoali-max <mafunzoali@gmail.com>
2026-07-14 15:39:11 +0300

    added

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,1 +1,64 @@
 
+# data "aws_ami" "os_image" { ... }   <- removed for Floci (no real AMI catalog behind it)
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "terra-automate-key"
+  public_key = file("terra-key.pub")
+}
+
+resource "aws_security_group" "allow_user_to_connect" {
+  name        = "allow TLS"
+  description = "Allow user to connect"
+  vpc_id      = module.vpc.vpc_id
+  dynamic "ingress" {
+    for_each = [
+      { description = "port 22 allow", from = 22, to = 22, protocol = "tcp", cidr = ["0.0.0.0/0"] },
+      { description = "port 80 allow", from = 80, to = 80, protocol = "tcp", cidr = ["0.0.0.0/0"] },
+      { description = "port 443 allow", from = 443, to = 443, protocol = "tcp", cidr = ["0.0.0.0/0"] },
+      { description = "port 8080 allow", from = 8080, to = 8080, protocol = "tcp", cidr = ["0.0.0.0/0"] }
+    ]
+    content {
+      description = ingress.value.description
+      from_port   = ingress.value.from
+      to_port     = ingress.value.to
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr
+    }
+  }
+
+  egress {
+    description = "allow all outgoing traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "mysecurity"
+  }
+}
+
+# NOTE: aws_instance is known to crash the Terraform AWS provider on Floci
+# during read-after-create (nil pointer panic). VPC/SG/subnet apply fine.
+# If apply dies here, comment this resource out and test the rest first.
+resource "aws_instance" "testinstance" {
+  ami                    = "ami-0c55b159cbfafe1f0" # any structurally valid AMI string works
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.allow_user_to_connect.id]
+  subnet_id              = module.vpc.public_subnets[0]
+  user_data              = file("${path.module}/install_tools.sh")
+  tags = {
+    Name = "Jenkins-Automate"
+  }
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+}
+
+resource "aws_eip" "jenkins_server_ip" {
+  instance = aws_instance.testinstance.id
+  domain   = "vpc"
+}
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,1 +1,25 @@
 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.18.1"
+
+  name            = local.name
+  cidr            = local.vpc_cidr
+  azs             = local.azs
+  public_subnets  = local.public_subnets
+  private_subnets = local.private_subnets
+
+  enable_nat_gateway     = false
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = false
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
+  map_public_ip_on_launch = true
+}
